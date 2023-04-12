ebad0c466118c467b22f0875e118685b5c33a53a
csh <csh0313c@gmail.com>
2023-04-12 18:44:57 +0900

    put integer

--- calc.py
+++ calc.py
@@ -3,18 +3,27 @@
 def plus(x, y):
     return x+y
 
+# check if it is integer
+def check_integer(s):
+    while True:
+        if s.isdigit():
+            return int(s)
+        else:
+            print("Put integer")
+            s = input()
+
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:        
+    while check >=1:       
         print("0: exit, 1: plus")
-        check = int(input())
+        check = check_integer(input())
         if check == 1:
             print("First Number")
-            x = int(input())
+            x = check_integer(input())
             print("Second Number")
-            y = int(input())
+            y = check_integer(input())
             print("answer : ", plus(x,y))
         elif check > 1:
             print("Unsupported")
